6b65c33e3b92da31e2af8578f230156b0b6a4089
himanshukandari14 <himanshukandari14@gmail.com>
2025-04-29 13:32:23 +0530

    added graceful shutdown

--- cmd/go-backend/main.go
+++ cmd/go-backend/main.go
@@ -1,8 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/himanshukandari14/go-server/internal/config"
+)
 
 
 func main() {
-	fmt.Println("j")
+	//load config
+	cfg:=config.MustLoad()
+	//db setup
+	//setup router
+	router :=http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome here"))
+	})
+	//setup srver
+	server :=http.Server{
+		Addr: cfg.Addr,
+		Handler: router,
+	}
+
+	fmt.Printf("server started %&",cfg.HTTPServer.Addr)
+
+	done:=make(chan os.Signal, 1)
+
+	signal.Notify(done,os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+
+	go func(){
+
+		err :=server.ListenAndServe()
+	
+		if err!=nil {
+			log.Fatal("failed tp start server")
+		}
+	}()
+
+	<- done //we will be blocked here 
+
+	slog.Info("shutting down the server")
+
+	ctx, cancel :=context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+
+	err :=server.Shutdown(ctx)
+
+	if err!=nil {
+		slog.Error("Failed to shutdownserver", slog.String("error", err.Error()))
+	}
+	slog.Info("Server shutodown sucessfully")
+
+
+
 }
