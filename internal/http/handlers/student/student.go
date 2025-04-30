d322b03df268c6602bb8b0d75f75ab665dda2b47
himanshukandari14 <himanshukandari14@gmail.com>
2025-05-01 04:17:47 +0530

    add get student by id

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log/slog"
 	"net/http"
+	"strconv"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/himanshukandari14/go-server/internal/storage"
@@ -51,5 +52,25 @@ func New(storage storage.Storage) http.HandlerFunc {
 		}
 
 		response.WriteJSON(w,http.StatusCreated,map[string]int64{"id":lastId})
+	}
+}
+
+func GetById(storage storage.Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		slog.Info("getting data of user",slog.String("id",id))
+		intId,err:=strconv.ParseInt(id,10,64)
+		if err!=nil{
+			response.WriteJSON(w,http.StatusBadRequest,response.GeneralError(err))
+		}
+		student,err:= storage.GetStudentById(intId)
+
+		if err!=nil{
+			response.WriteJSON(w,http.StatusInternalServerError, response.GeneralError(err))
+			return
+		}
+
+		response.WriteJSON(w,http.StatusOK,student)
+
 	}
 }
--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -2,8 +2,10 @@ package sqlite
 
 import (
 	"database/sql"
+	"fmt"
 
 	"github.com/himanshukandari14/go-server/internal/config"
+	"github.com/himanshukandari14/go-server/internal/types"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -11,14 +13,13 @@ type Sqlite struct {
 	Db *sql.DB
 }
 
-
 func New(cfg config.Config) (*Sqlite, error) {
-	db, err := sql.Open("sqlite3",cfg.StoragePath)
+	db, err := sql.Open("sqlite3", cfg.StoragePath)
 	if err != nil {
 		return nil, err
 	}
 
-	_, err =db.Exec(`CREATE TABLE IF NOT EXISTS students (
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS students (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	name TEXT,
 	email TEXT,
@@ -26,32 +27,57 @@ func New(cfg config.Config) (*Sqlite, error) {
 	)`)
 
 	if err != nil {
-		return nil,err
+		return nil, err
 
 	}
 
 	return &Sqlite{
-		Db:db,
+		Db: db,
 	}, nil
 }
 
-func (s *Sqlite)CreateStudent(name string, email string, age int) (int64, error){
-
-	stmt, err:=s.Db.Prepare("INSERT INTO STUDENTS (name, email, age) VALUES (?, ?, ?)")
-	if err!= nil {
+func (s *Sqlite) CreateStudent(name string, email string, age int) (int64, error) {
+	res, err := s.Db.Exec("INSERT INTO students (name, email, age) VALUES (?, ?, ?)", name, email, age)
+	if err != nil {
 		return 0, err
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
+
+func (s *Sqlite) GetStudentById(id int64) (types.Student, error) {
+	var student types.Student
+
+	err := s.Db.QueryRow("SELECT id, name, email, age FROM students WHERE id = ?", id).
+		Scan(&student.Id, &student.Name, &student.Email, &student.Age)
+
+	return student, err
+}
+
+func (s *Sqlite) GetById(id int64) (types.Student, error) {
+	stmt, err := s.Db.Prepare("SELECT * FROM STUDENTS WHERE ID = ? LIMIT 1")
+
+	if err != nil {
+		return types.Student{}, err
 	}
 
 	defer stmt.Close()
 
-	result, err:=stmt.Exec(name, email, age)
-	if err!=nil {
-		return 0,err
-	}
+	var student types.Student
 
-	lastId, err :=result.LastInsertId()
-	if err!=nil{
-		return 0,err
+	err = stmt.QueryRow(id).Scan(&student.Id, &student.Email, &student.Age)
+
+	if err != nil {
+
+		if err == sql.ErrNoRows {
+			return types.Student{}, fmt.Errorf("no student found with id %w", fmt.Sprint(id))
+		}
+		return types.Student{}, fmt.Errorf("query error %w", err)
 	}
-	return lastId, nil
+	return student, nil
 }
