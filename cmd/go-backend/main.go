842cdf6d786ddf73d7bf99986f9bcdf199cbc92b
himanshukandari14 <himanshukandari14@gmail.com>
2025-05-01 03:46:47 +0530

    database

--- cmd/go-backend/main.go
+++ cmd/go-backend/main.go
@@ -12,54 +12,57 @@ import (
 
 	"github.com/himanshukandari14/go-server/internal/config"
 	"github.com/himanshukandari14/go-server/internal/http/handlers/student"
+	"github.com/himanshukandari14/go-server/internal/storage/sqlite"
 )
-
 
 func main() {
 	//load config
-	cfg:=config.MustLoad()
-	//db setup
+	cfg := config.MustLoad()
+	//db setups
+	storage, err := sqlite.New(*cfg)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	slog.Info("storage initilaized", slog.String("env", cfg.Env), slog.String("version", "1,0"))
+
 	//setup router
-	router :=http.NewServeMux()
-	router.HandleFunc("POST /api/students",student.New())
-	
+	router := http.NewServeMux()
+	router.HandleFunc("POST /api/students", student.New(storage))
+
 	//setup srver
-	server :=http.Server{
-		Addr: cfg.Addr,
+	server := http.Server{
+		Addr:    cfg.Addr,
 		Handler: router,
 	}
 
-	slog.Info("Server started",slog.String("address",cfg.Addr))
+	slog.Info("Server started", slog.String("address", cfg.Addr))
 
-	done:=make(chan os.Signal, 1)
+	done := make(chan os.Signal, 1)
 
-	signal.Notify(done,os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
+	go func() {
 
-	go func(){
+		err := server.ListenAndServe()
 
-		err :=server.ListenAndServe()
-	
-		if err!=nil {
+		if err != nil {
 			log.Fatal("failed tp start server")
 		}
 	}()
 
-	<- done //we will be blocked here 
+	<-done //we will be blocked here
 
 	slog.Info("shutting down the server")
 
-	ctx, cancel :=context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-
-	err :=server.Shutdown(ctx)
-
-	if err!=nil {
-		slog.Error("Failed to shutdownserver", slog.String("error", err.Error()))
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
 	}
-	slog.Info("Server shutodown sucessfully")
 
-
+	slog.Info("server shutdown successfully")
 
 }
--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -9,12 +9,13 @@ import (
 	"net/http"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/himanshukandari14/go-server/internal/storage"
 	"github.com/himanshukandari14/go-server/internal/types"
 	response "github.com/himanshukandari14/go-server/internal/utils/responses"
 )
 
 
-func New() http.HandlerFunc {
+func New(storage storage.Storage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		slog.Info("creating student")
 
@@ -38,6 +39,17 @@ func New() http.HandlerFunc {
 			return
 		}
 
-		response.WriteJSON(w,http.StatusCreated,map[string]string{"success":"ok"})
+		lastId,err:=storage.CreateStudent(
+			student.Name,
+			student.Email,
+			student.Age,
+		)
+		slog.Info("user created succesfully",slog.String("userId",fmt.Sprint(lastId)))
+		if err!=nil {
+			response.WriteJSON(w,http.StatusInternalServerError, err)
+			return
+		}
+
+		response.WriteJSON(w,http.StatusCreated,map[string]int64{"id":lastId})
 	}
 }
