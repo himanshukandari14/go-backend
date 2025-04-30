7a03b245f87e06adc6526b375f7e4d27dd764d5e
himanshukandari14 <himanshukandari14@gmail.com>
2025-05-01 04:37:38 +0530

    add students list

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -74,3 +74,18 @@ func GetById(storage storage.Storage) http.HandlerFunc {
 
 	}
 }
+
+func GetList(storage storage.Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("getting all students")
+		students, err:=storage.GetStudents()
+
+		if err !=nil{
+			response.WriteJSON(w,http.StatusInternalServerError,err)
+			return
+		}
+
+		response.WriteJSON(w,http.StatusOK,students)
+
+	}
+}
